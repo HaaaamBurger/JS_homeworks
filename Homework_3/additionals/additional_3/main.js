c3f48d6782808e348f751e77b97210caa7da68a1
HaaaamBurger <gemasclashes@gmail.com>
2023-05-09 18:27:11 +0300

    #

--- Homework_3/additionals/additional_3/main.js
+++ Homework_3/additionals/additional_3/main.js
@@ -135,7 +135,7 @@
 //     {user_id: 4, country: 'USA', city: 'Miami'}
 // ];
 //
-// З'єднати в один об'єкт користувача та місто з відповідними "id" та "user_id" .
+// З'єднати в один об'єкт користувача та місто з відповідними "id" та "user_id".
 //     Записати цей об'єкт в новий масив
 // Example:
 //     let usersWithCities = [
@@ -153,6 +153,38 @@
 //         // TO BE CONTINUED .....
 //     ]
 //----------------------------------------------------------------------------------------------------------------------
+// let usersWithId = [
+//     {id: 1, name: 'vasya', age: 31, status: false},
+//     {id: 2, name: 'petya', age: 30, status: true},
+//     {id: 3, name: 'kolya', age: 29, status: true},
+//     {id: 4, name: 'olya', age: 28, status: false}
+// ];
+//
+// let citiesWithId = [
+//     {user_id: 3, country: 'USA', city: 'Portland'},
+//     {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+//     {user_id: 2, country: 'Poland', city: 'Krakow'},
+//     {user_id: 4, country: 'USA', city: 'Miami'}
+// ];
+// let newArr = [];
+// for (let i = 0; i < usersWithId.length; i++) {
+//     for (let j = 0; j < citiesWithId.length; j++) {
+//         if (usersWithId[i].id === citiesWithId[j].user_id) {
+//             newArr[i] = {
+//                 id: usersWithId[i].id,
+//                 name: usersWithId[i].name,
+//                 age: usersWithId[i].age,
+//                 status: usersWithId[i].status,
+//                 address: {
+//                     user_id: citiesWithId[j].user_id,
+//                     country: citiesWithId[j].country,
+//                     city: citiesWithId[j].city
+//                 }
+//             }
+//         }
+//     }
+// }
+// console.log(newArr);
 //----------------------------------------------------------------------------------------------------------------------
 //----------------------------------------------------------------------------------------------------------------------
 //----------------------------------------------------------------------------------------------------------------------
