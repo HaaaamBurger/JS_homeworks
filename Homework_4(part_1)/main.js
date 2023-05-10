0d6f7335a3f09533ddccd671676459ab37fab994
HaaaamBurger <gemasclashes@gmail.com>
2023-05-11 01:44:34 +0300

    #

--- Homework_4(part_1)/main.js
+++ Homework_4(part_1)/main.js
@@ -158,4 +158,30 @@
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
 //----------------------------------------------------------------------------------------------------------------------
+// function exchange(sumUAH,currencyValues,exchangeCurrency) {
+//     let money = {sumUAH, currencyValues, exchangeCurrency};
+//
+//     return {
+//         getValues: function () {
+//             return {sumUAH,currencyValues, exchangeCurrency};
+//         },
+//         setValues: function () {
+//             if (sumUAH > 0 && currencyValues && exchangeCurrency > 0) {
+//                 return 'All values are appropriate!';
+//             } else {
+//                 return 'Something went wrong!';
+//             }
+//         },
+//         calculateValues: function () {
+//             if (this.setValues) {
+//                 let result = sumUAH / exchangeCurrency;
+//                 return `Value: ${sumUAH} UAH\nExchange currency: ${exchangeCurrency} ${currencyValues}\nResult: ${result} UAH`
+//             } else {
+//                 return 'Values are inappropriate!';
+//             }
+//         }
+//     }
+// }
+// let manipulator = exchange(500, 'USD', 42);
+// console.log(manipulator.calculateValues());
 //----------------------------------------------------------------------------------------------------------------------
