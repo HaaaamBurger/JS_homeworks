98165d9349c4581107087dfca53d564d5b1e60da
HaaaamBurger <gemasclashes@gmail.com>
2023-05-18 15:02:59 +0300

    #

--- Homework_5/main.js
+++ Homework_5/main.js
@@ -220,3 +220,58 @@
 //     { cardSuit: 'clubs', value: 'ace', color: 'black' },
 // ];
 //----------------------------------------------------------------------------------------------------------------------
+// console.log(cards.reduce((accumulator, card) => {
+//     if (card.cardSuit === 'spade') {
+//         accumulator.spades.push(card);
+//     } else if (card.cardSuit === 'diamond') {
+//         accumulator.diamonds.push(card);
+//     } else if (card.cardSuit === 'heart') {
+//         accumulator.hearts.push(card);
+//     } else {
+//         accumulator.clubs.push(card);
+//     }
+//     return accumulator;
+// }, {spades: [], diamonds: [], hearts: [], clubs: []}))
+//----------------------------------------------------------------------------------------------------------------------
+let cards = [
+    { cardSuit: 'spade', value: '6', color: 'black' },
+    { cardSuit: 'diamond', value: '6', color: 'red' },
+    { cardSuit: 'heart', value: '6', color: 'red' },
+    { cardSuit: 'clubs', value: '6', color: 'black' },
+    { cardSuit: 'spade', value: '7', color: 'black' },
+    { cardSuit: 'diamond', value: '7', color: 'red' },
+    { cardSuit: 'heart', value: '7', color: 'red' },
+    { cardSuit: 'clubs', value: '7', color: 'black' },
+    { cardSuit: 'spade', value: '8', color: 'black' },
+    { cardSuit: 'diamond', value: '8', color: 'red' },
+    { cardSuit: 'heart', value: '8', color: 'red' },
+    { cardSuit: 'clubs', value: '8', color: 'black' },
+    { cardSuit: 'spade', value: '9', color: 'black' },
+    { cardSuit: 'diamond', value: '9', color: 'red' },
+    { cardSuit: 'heart', value: '9', color: 'red' },
+    { cardSuit: 'clubs', value: '9', color: 'black' },
+    { cardSuit: 'spade', value: '10', color: 'black' },
+    { cardSuit: 'diamond', value: '10', color: 'red' },
+    { cardSuit: 'heart', value: '10', color: 'red' },
+    { cardSuit: 'clubs', value: '10', color: 'black' },
+    { cardSuit: 'spade', value: 'jack', color: 'black' },
+    { cardSuit: 'diamond', value: 'jack', color: 'red' },
+    { cardSuit: 'heart', value: 'jack', color: 'red' },
+    { cardSuit: 'clubs', value: 'jack', color: 'black' },
+    { cardSuit: 'spade', value: 'queen', color: 'black' },
+    { cardSuit: 'diamond', value: 'queen', color: 'red' },
+    { cardSuit: 'heart', value: 'queen', color: 'red' },
+    { cardSuit: 'clubs', value: 'queen', color: 'black' },
+    { cardSuit: 'spade', value: 'king', color: 'black' },
+    { cardSuit: 'diamond', value: 'king', color: 'red' },
+    { cardSuit: 'heart', value: 'king', color: 'red' },
+    { cardSuit: 'clubs', value: 'king', color: 'black' },
+    { cardSuit: 'spade', value: 'ace', color: 'black' },
+    { cardSuit: 'diamond', value: 'ace', color: 'red' },
+    { cardSuit: 'heart', value: 'ace', color: 'red' },
+    { cardSuit: 'clubs', value: 'ace', color: 'black' },
+];
+//----------------------------------------------------------------------------------------------------------------------
+// --написати пошук всіх об'єктів, в який в modules є sass
+// --написати пошук всіх об'єктів, в який в modules є docker
+//----------------------------------------------------------------------------------------------------------------------
