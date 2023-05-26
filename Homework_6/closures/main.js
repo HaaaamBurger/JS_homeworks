768800f197f5c5af7a2d2fa1f94ae6a9866b06c2
HaaaamBurger <gemasclashes@gmail.com>
2023-05-27 00:36:57 +0300

    #

--- Homework_6/closures/main.js
+++ Homework_6/closures/main.js
@@ -12,6 +12,7 @@ function UserCard(number) {
 
     this.putCredits = function (credits) {
         cardTemplate.balance += credits;
+        cardTemplate.historyLogs.push({operationType: 'Received credits', credits: credits, operationTime: new Date()});
     };
 
     this.takeCredits = function (credits) {
@@ -20,6 +21,9 @@ function UserCard(number) {
         } else {
             console.error(`Transaction limit is : ${cardTemplate.transactionLimit}.`);
         }
+
+
+        cardTemplate.historyLogs.push({operationType: 'Withdrawal of credits', credits: credits, operationTime: new Date()});
     };
     this.setTransactionLimit = function (credits) {
         if (credits >= 0) {
@@ -27,6 +31,8 @@ function UserCard(number) {
         } else {
             console.error('You cannot set transaction limit less than 0 credits !');
         }
+        const date = new Date();
+        cardTemplate.historyLogs.push({operationType: 'Transaction limit change', credits: credits, operationTime: date});
     };
 
     this.transferCredits = function (credits, recipient) {
@@ -40,7 +46,46 @@ function UserCard(number) {
             console.error('Something went wrong !');
         }
     }
-
 }
-const card = new UserCard(1);
-const card1 = new UserCard(2);
+
+function UserAccount(name) {
+    this.name = name;
+    this.cards = [];
+    this.addCard = function (number) {
+        if (this.cards.length < 3) {
+            this.cards.push(new UserCard(number));
+        } else {
+            console.error('Maximum cards limit reached !(3)');
+        }
+    };
+    this.getCardById = function (cardKey) {
+        switch (cardKey) {
+            case 1:
+                return this.cards[0].getCardOptions();
+            case 2:
+                return this.cards[1].getCardOptions();
+            case 3:
+                return this.cards[2].getCardOptions();
+            default:
+                console.error('Something went wrong !');
+        }
+    }
+}
+let user = new UserAccount('Bob');
+user.addCard(1);
+user.addCard(2);
+
+let card1 = user.getCardById(1);
+let card2 = user.getCardById(2);
+
+card1.putCredits(500);
+card1.setTransactionLimit(800);
+card1.transferCredits(300,card2);
+
+card2.takeCredits(50);
+
+console.log(card1.getCardOptions());
+
+
+
+
