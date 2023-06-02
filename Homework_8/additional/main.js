add35d8b6c62324dce61bd5cbb7eff7b271a658f
HaaaamBurger <gemasclashes@gmail.com>
2023-06-02 14:52:01 +0300

    #

--- Homework_8/additional/main.js
+++ Homework_8/additional/main.js
@@ -29,27 +29,24 @@
 //     зміна ціни відбувається тільки на перезавантаження, які відбулись пізніше ніж 10 секунд після попереднього.
 //     При перезавантаженні, яке відбулось раніше ніж минуло 10 секунд - нічого не відбувається
 //======================================================================================================================
-const a = document.getElementById('setMoney');
-
-window.onload = () => {
-    let generalMount = JSON.parse(localStorage.getItem('money')) || 100;
-    a.innerHTML = `${generalMount}`;
-    const mainTime = new Date().getTime();
-
-    let openTime = JSON.parse(localStorage.getItem('openTime')) || [];
-    openTime.push(mainTime);
-
-    if (openTime.length > 1) {
-        if (openTime[openTime.length - 1] - openTime[openTime.length - 2] >= 10000) {
-            generalMount += 20;
-            a.innerHTML = `${generalMount}`;
-            localStorage.setItem('money', JSON.stringify(generalMount));
-        }
-    }
-
-    localStorage.setItem('openTime', JSON.stringify(openTime));
-
-}
-
-
+// const a = document.getElementById('setMoney');
+//
+// window.onload = () => {
+//     let generalMount = JSON.parse(localStorage.getItem('money')) || 100;
+//     a.innerHTML = `${generalMount}`;
+//     const mainTime = new Date().getTime();
+//
+//     let openTime = JSON.parse(localStorage.getItem('openTime')) || [];
+//     openTime.push(mainTime);
+//
+//     if (openTime.length > 1) {
+//         if (openTime[openTime.length - 1] - openTime[openTime.length - 2] >= 10000) {
+//             generalMount += 20;
+//             a.innerHTML = `${generalMount}`;
+//             localStorage.setItem('money', JSON.stringify(generalMount));
+//         }
+//     }
+//
+//     localStorage.setItem('openTime', JSON.stringify(openTime));
+// }
 //======================================================================================================================
