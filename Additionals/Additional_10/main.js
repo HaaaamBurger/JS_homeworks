fda8bbc88d91c8ecd7dcae91b4b29cb3a70cc9b0
HaaaamBurger <gemasclashes@gmail.com>
2023-06-09 17:14:49 +0300

    #

--- Additionals/Additional_10/main.js
+++ Additionals/Additional_10/main.js
@@ -42,4 +42,43 @@
 //---------------------------------------------------
 // - Создайте меню, которое раскрывается/сворачивается при клике
 //---------------------------------------------------
+// const mainMenu  = document.getElementsByClassName('mainMenu')[0];
+// const buttonMenu = document.getElementsByClassName('buttonMenu')[0];
+// const navElements = document.getElementsByClassName('navElements')[0];
+//
+// const div_1 = document.createElement('div');
+// div_1.innerText = '1';
+// div_1.classList.add('buttonMenu','specialClass');
+// const div_2 = document.createElement('div');
+// div_2.innerText = '2';
+// div_2.classList.add('buttonMenu','specialClass');
+// const div_3 = document.createElement('div');
+// div_3.innerText = '3';
+// div_3.classList.add('buttonMenu','specialClass');
+// const div_4 = document.createElement('div');
+// div_4.innerText = '4';
+// div_4.classList.add('buttonMenu','specialClass');
+// const div_5 = document.createElement('div');
+// div_5.innerText = '5';
+// div_5.classList.add('buttonMenu','specialClass');
+// div_5.style.borderRadius = '0 0 3px 3px';
+//
+// let counter = 0;
+// mainMenu.onclick = () => {
+//     if (counter % 2 === 0) {
+//         navElements.append(div_1,div_2,div_3,div_4,div_5);
+//         counter++;
+//         console.log(counter);
+//     } else {
+//         div_1.remove();
+//         div_2.remove();
+//         div_3.remove();
+//         div_4.remove();
+//         div_5.remove();
+//         counter++;
+//         console.log(counter);
+//     }
+// }
+
+
 //---------------------------------------------------
